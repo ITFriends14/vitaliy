d5f909a2ba25d7cb4c4d891f323e47705eef0b51
ITFriends14 <128975094+ITFriends14@users.noreply.github.com>
2023-03-26 18:10:24 +0300

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,51 @@
+
+import turtle
+#buba = {'cat': 'muay', 'dog': 'gav', 'cow': 'muuu'}
+#print(buba['cat'])
+
+
+#for i in range(10, 0, -1):
+#    print(i)
+
+#buba =  input("Ваше имя: ")
+
+#while buba != 'Vlad':
+#    buba = input("Ваше имя: ")
+
+#num = 10
+#while num: 
+#    print(num)
+#    num -= 1 
+
+
+#buba = 10
+#while buba:
+#    for i in range(buba):
+#       print(buba)
+#      buba -= 1
+
+
+#buba = 1
+#while buba:
+ #   buba = int(eval(input("чтоб прекратить програму напишите 0   "))) 
+ #   print(buba) 
+#buba = [1, 2, 3, 67, 8089, -2]
+#for i in buba: 
+ #   print(i)
+
+#nums = [1, 2, 3, 67, 8089, -2]
+#num = 0
+#for i in nums:
+#   num += i
+#print(num)     
+
+turtle.bgcolor('red')
+turtle.begin_fill()
+#turtle.color('red', 'yellow')
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+for i in range(60):
+    turtle.forward(5)
+    turtle.right(8)
+
